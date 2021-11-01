dbd644a794b8d73aacbeec5dcf3476f012a51265
Raj Dubey <55090463+rajdubey09@users.noreply.github.com>
2021-11-01 17:51:15 +0530

    Update Bubble Sort.py

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -4,20 +4,22 @@ def bubble_sort(number_list):
     size = len(number_list)
     
     for i in range(size-1):
-        swapped = False
+        #swapped = False
         for j in range(size-1-i):
             if number_list[j] > number_list[j+1]:
                 tmp = number_list[j]
                 number_list[j] = number_list[j+1]
                 number_list[j+1] = tmp
-                swapped = True
+                #swapped = True
 
-        if not swapped:
-            break
+         #if not swapped:
+             #break
+            
+    return number_list            
 
 if __name__ == '__main__':
 
-    #number_list = [5,9,2,1,67,34,88,34,8,7,9]
-    number_list = [1,2,7,8,9]
-    bubble_sort(number_list)
-    print(number_list)
+    number_list = [5,9,2,1,67,34,88,34,8,7,9]
+    #number_list = [1,2,7,8,9]
+    res = bubble_sort(number_list)
+    print("Sorted List",res)
